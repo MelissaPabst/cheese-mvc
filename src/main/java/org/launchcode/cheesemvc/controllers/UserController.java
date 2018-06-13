9fd6037e4521cc246662a763463ec217eb43d868
Melissa Pabst <melissapabst@gmail.com>
2018-06-12 22:48:18 -0500

    user signup continued: add model validation

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -4,11 +4,15 @@ package org.launchcode.cheesemvc.controllers;
 import org.launchcode.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.launchcode.cheesemvc.models.UserData;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.validation.Valid;
+
 
 
 
@@ -29,23 +33,27 @@ public class UserController {
     @RequestMapping(value="add", method = RequestMethod.GET)
     public String displayAddForm(Model model) {
         model.addAttribute("title", "Add User");
+        //skeleton user properties will help us render form properly
+        //same as model.addAttribute(user new User()); default without "user" creates lower-case version of User class
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddForm(Model model, @ModelAttribute @Valid User newUser, Errors errors) {
 
-        if (user.getPassword() != null && user.getPassword().equals(verify)) {
-            model.addAttribute("name", user.getUsername());
-            UserData.add(user);
-            return "user/index";
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add User");
+            model.addAttribute(newUser);
+            model.addAttribute("username", newUser.getUsername());
+            model.addAttribute("email", newUser.getEmail());
+            return "user/add";
         }
 
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("error", "Passwords do not match");
-        return "user/add";
-
+        UserData.add(newUser);
+        model.addAttribute("user", newUser);
+        model.addAttribute("title", "All Users");
+        return "redirect:";
     }
 
     @RequestMapping(value = "{userId}")
